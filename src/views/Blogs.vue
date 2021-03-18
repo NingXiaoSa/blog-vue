7bd1e80549b7d6cdaa8dc2380b288174c718fe11
Roamantic <1518622781@qq.com>
2021-03-18 09:55:30 +0800

    2021.3.18 bug修复

--- src/views/Blogs.vue
+++ src/views/Blogs.vue
@@ -4,17 +4,26 @@
     <Header></Header>
     <div class="block">
       <el-timeline>
-        <el-timeline-item timestamp="2018/4/12" placement="top">
+        <el-timeline-item :timestamp="blog.created" placement="top" v-for="blog in blogs">
           <el-card>
-            <h4>更新 Github 模板</h4>
-            <p>王小虎 提交于 2018/4/12 20:46</p>
+
+            <h4>
+              <router-link :to="{name:'BlogDetail',params:{blogId:blog.id}}">
+                {{ blog.title }}
+              </router-link>
+            </h4>
+
+            <p>{{ blog.description }}</p>
           </el-card>
         </el-timeline-item>
       </el-timeline>
       <el-pagination class="mpage"
                      background
                      layout="prev, pager, next"
-                     :total="1000">
+                     :current-page="currentPage"
+                     :page-size="pageSize"
+                     :total="total"
+                     @current-change=page>
       </el-pagination>
     </div>
   </div>
@@ -37,14 +46,17 @@ export default {
   methods: {
     page(currentPage) {
       const _this = this
-      _this.$axios().get("/blogs?currentPage=" + currentPage).then(res => {
+      _this.$axios.get("/blogs?currentPage=" + currentPage).then(res => {
         console.log(res)
         _this.blogs = res.data.data.records
-        _this.currentPage=res.data.data.current
-        _this.total=res.data.data.total
-        _this.pagesize=res.data.data.size
+        _this.currentPage = res.data.data.current
+        _this.total = res.data.data.total
+        _this.pagesize = res.data.data.size
       })
     }
+  },
+  created() {
+    this.page(1)
   }
 }
 
